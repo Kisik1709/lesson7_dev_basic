73d949db1d329249557d405612a33b24c7c8f8ca
Kisik1709 <themegadimasssik1@gmail.com>
2025-05-02 15:57:38 +0700

    Update lesson7_dev_basic_bot.py

--- lesson7_dev_basic_bot.py
+++ lesson7_dev_basic_bot.py
@@ -2,11 +2,6 @@ import os
 import ptbot
 from dotenv import load_dotenv
 from pytimeparse import parse
-load_dotenv()
-
-
-TG_TOKEN = os.getenv("API_TG")
-TG_CHAT_ID = os.getenv("TG_CHAT_ID")
 
 
 def render_progressbar(total, iteration, prefix='', suffix='', length=30, fill='█', zfill='░'):
@@ -18,15 +13,15 @@ def render_progressbar(total, iteration, prefix='', suffix='', length=30, fill='
     return '{0} |{1}| {2}% {3}'.format(prefix, pbar, percent, suffix)
 
 
-def wait(chat_id, question):
+def wait(bot, chat_id, question):
     time = parse(question)
     message_id = bot.send_message(chat_id, "Запускаю обратный отсчет")
-    bot.create_countdown(time, notify_progress,
+    bot.create_countdown(time, notify_progress, bot=bot,
                          chat_id=chat_id, message_id=message_id, time=time)
-    bot.create_timer(time, choose, chat_id=chat_id, question=question)
+    bot.create_timer(time, choose, bot=bot, chat_id=chat_id, question=question)
 
 
-def notify_progress(secs_left, chat_id, message_id, time):
+def notify_progress(secs_left, bot, chat_id, message_id, time):
     text = (
         f"Осталось секунд: {secs_left}\n"
         f"{render_progressbar(time, time - secs_left)}"
@@ -34,12 +29,18 @@ def notify_progress(secs_left, chat_id, message_id, time):
     bot.update_message(chat_id, message_id, text)
 
 
-def choose(chat_id, question):
+def choose(bot, chat_id, question):
     message = "Время вышло!"
     bot.send_message(chat_id, message)
 
 
-if __name__ == "__main__":
-    bot = ptbot.Bot(TG_TOKEN)
-    bot.reply_on_message(wait)
+def main():
+    load_dotenv()
+    tg_token = os.getenv("API_TG")
+    bot = ptbot.Bot(tg_token)
+    bot.reply_on_message(lambda chat_id, msg: wait(bot, chat_id, msg))
     bot.run_bot()
+
+
+if __name__ == "__main__":
+    main()
